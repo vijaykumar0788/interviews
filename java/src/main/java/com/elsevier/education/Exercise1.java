202a90427fbd75edd4620e920fe0c955717cb5a4
Patel, Vijay Kumar (Contractor) VijayKumar_Patel@cable.comcast.com <VijayKumar_Patel@cable.comcast.com>
2017-08-01 02:24:08 -0400

    created immutable class as part Exercise1

--- java/src/main/java/com/elsevier/education/Exercise1.java
+++ java/src/main/java/com/elsevier/education/Exercise1.java
@@ -1,5 +1,7 @@
 package com.elsevier.education;
 
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -7,36 +9,74 @@ import java.util.Set;
 TODO: Make this class immutable.
 
 */
-public class Exercise1 {
-
+public final class Exercise1 {
+	
+	 
+	public Person getPerson(){
+		return Exercise1.Person.person;
+	}
+	
 	public static class Person {
 		
-		private Set<String> phoneNumbers;
-		private String firstName;
-		private String lastName;
+		private static Person person;
 		
-		public Person() {
+		private final Set<String> phoneNumbers; 
+		private final String firstName;
+		private final String lastName;
+		
+		/*
+		 * Make this constructor private to avoid unplanned object creation
+		 */
+		private Person(Set<String> phoneNumbers, String firstName, String lastName) {
+			this.phoneNumbers = new HashSet<String>(phoneNumbers);
+			this.firstName = firstName;
+			this.lastName = lastName;
 		}
+		
 
+		/* 
+		 * createPerson method created to have 
+		 * object creation logic in single place
+		 */
+		public static void createPerson(Set<String> phoneNumbers, String firstName, String lastName)
+	    {
+			if(person == null ){
+				synchronized (Person.class) {
+					if(person == null ){
+						person = new Person(phoneNumbers, firstName, lastName);
+					}
+				}
+			}
+	    }
+		
 		public Set<String> getPhoneNumbers() {
-			return phoneNumbers;
-		}
-		public void setPhoneNumbers(Set<String> newPhoneNumbers) {
-			phoneNumbers = newPhoneNumbers;
+			return Collections.unmodifiableSet(phoneNumbers);
 		}
 		
 		public String getFirstName() {
 			return firstName;
 		}
-		public void setFirstName(String newName) {
-			firstName = newName;
-		}
 		
 		public String getLastName() {
 			return lastName;
 		}
+		
+		/*
+		 * AS part of making this class immutable 
+		 * there should not be any setter method
+		 * We can remove below commented methods 
+		 */
+		/*public void setPhoneNumbers(Set<String> newPhoneNumbers) {
+			phoneNumbers = newPhoneNumbers;
+		}
+		
+		public void setFirstName(String newName) {
+			firstName = newName;
+		}
+		
+		
 		public void setLastName(String newName) {
 			lastName = newName;
-		}
+		}*/
 	}
 }
